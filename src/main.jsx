d77577c33a6cefb2d951878ff916562a678c34d4
Anurag Singh <anurags9693@gmail.com>
2025-01-22 20:41:01 +0500

    j

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+// import App from './App.jsx'
 // import UserContext from './context/UserContext.jsx'
 // import {CaptainProvider }from './context/CaptainContext.jsx'
 
@@ -9,7 +9,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* <UserContext>
       <CaptainProvider> */}
-        <App />
+        {/* <App /> */}
+        <h1>hey there </h1>
       {/* </CaptainProvider>
     </UserContext> */}
   </StrictMode>
