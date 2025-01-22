39b25b598f3f1de1a533c6b6ecf09cec5d5326d3
Anurag Singh <anurags9693@gmail.com>
2025-01-22 13:58:15 +0500

    ll

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.js'
+import App from './App.jsx'
 // import UserContext from './context/UserContext.jsx'
 // import {CaptainProvider }from './context/CaptainContext.jsx'
 
